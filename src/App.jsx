2420f435b5e47002770bbfab26243b2afb3f6efa
husanovjamshid <husanovjamshid11@gmail.com>
2023-02-22 22:14:01 +0500

    add banner page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { fetchDataFromAPI } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { Header } from "./components/header/Header";
 import { Footer } from "./components/footer/Footer";
 import { Home } from "./pages/home/Home";
@@ -11,28 +13,40 @@ import { SearchResult } from "./pages/searchResult/SearchResult";
 import { Explore } from "./pages/explore/Explore";
 import { pageNotFound } from "./pages/404/PageNotFound";
 
-
 function App() {
   const dispatch = useDispatch();
   const { url } = useSelector((state) => state.home);
 
-  const apiTesting = () => {
-    fetchDataFromAPI("/movie/popular").then((data) => {
+  const fetchApiConfig = () => {
+    fetchDataFromAPI("/configuration").then((data) => {
       console.log(data);
-      dispatch(getApiConfiguration(data));
+      const url = {
+        backdrop: data.images.secure_base_url + "original",
+        poster: data.images.secure_base_url + "original",
+        profile: data.images.secure_base_url + "original",
+      };
+
+      dispatch(getApiConfiguration(url));
     });
   };
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
   return (
-    <div className="App">
-      App
-      {url?.total_pages}
-    </div>
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<pageNotFound />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App
