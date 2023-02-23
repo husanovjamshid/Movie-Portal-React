b50be51bec56bd4c66ee5069a3682af25f5b1756
husanovjamshid <husanovjamshid11@gmail.com>
2023-02-23 19:00:33 +0500

    add carousel

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { HeroBanner } from "./heroBanner/HeroBanner";
 import "./style.scss";
+import { Trending } from "./trending/Trending";
 
 HeroBanner;
 
@@ -8,6 +9,7 @@ export const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
+      <Trending />
       <div style={{ height: 1000 }}></div>
     </div>
   );
