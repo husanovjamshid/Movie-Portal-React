e76e514096c1e09aad3f5a90c5d3e609023523d1
husanovjamshid <husanovjamshid11@gmail.com>
2023-02-24 17:52:05 +0500

    add details page and play button

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import { DetailsBanner } from "./detailsBanner/DetailsBanner";
+import "./style.scss";
 export const Details = () => {
+  // const { mediaType, id } = useParams();
+  // const { data, loading } = useFetch(`/${mediaType}/${id}`);
+
   return (
-    <div>Details</div>
-  )
-}
+    <div>
+      <DetailsBanner />
+    </div>
+  );
+};
